bb1111383cd776eb207c718dfd28ac1fdfa79ab7
Taro Morita <morita0079@gmail.com>
2022-07-11 02:30:47 +0900

    first commit

---
+++ src/main.zig
@@ -0,0 +1,36 @@
+const std = @import("std");
+const print = std.debug.print;
+
+pub fn main() void {
+    // integer
+    const one_plus_one: i32 = 1 + 1;
+    print("1 + 1 = {}\n", .{one_plus_one});
+
+    // float
+    const seven_div_three: f32 = 7.0 / 3.0;
+    print("7.0 / 3.0 = {}\n", .{seven_div_three});
+
+    // boolean
+    print("{}\n{}\n{}\n", .{
+        true and false,
+        true or false,
+        !true,
+    });
+
+    // optional
+    var optional_value: ?[]const u8 = null;
+    assert(optional_value == null);
+
+    print("\noptional 1\ntype: {s}\nvalue: {s}\n", .{
+        @typeName(@Typeof(optional_value)),
+        optional_value,
+    });
+
+    optional_value = "hi";
+    assert(optional_value != null);
+
+    print("\noptional 2\ntype: {s}\nvalue: {s}\n", .{
+        @typeName(@TypeOf(optional_value)),
+        optional_value,
+    });
+}
